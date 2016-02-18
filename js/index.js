df4c162331345169b8ebeb895d43b3e09da39bbc
Yoo-Hello <765711543@qq.com>
2016-02-18 22:34:20 +0800

    增加了重新开始

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+	var resetting=document.querySelector('#resetting');
 	var canvas=document.querySelector('#canvas');
 	var ctx=canvas.getContext('2d');
 
@@ -16,47 +17,50 @@ window.onload=function(){
 	// ctx.stroke();
 
 	for(var i=0;i<15;i++){
-       ctx.beginPath();
-       ctx.moveTo(20.5+i*40,20);
-       ctx.lineTo(20.5+i*40,580);
-       ctx.stroke(); 
+       bgc.beginPath();
+       bgc.moveTo(20.5+i*40,20);
+       bgc.lineTo(20.5+i*40,580);
+       bgc.stroke(); 
 
-       ctx.beginPath();
-       ctx.moveTo(20,20.5+i*40);
-       ctx.lineTo(580,20.5+i*40);
-       ctx.stroke();
+       bgc.beginPath();
+       bgc.moveTo(20,20.5+i*40);
+       bgc.lineTo(580,20.5+i*40);
+       bgc.stroke();
 	}
 
-    ctx.beginPath();
-	ctx.arc(300,300,3,0,Math.PI*2);
-	ctx.fill();
+    bgc.beginPath();
+
+	bgc.fillStyle="rgb(0,0,0)";
+	bgc.arc(300,300,3,0,Math.PI*2);
+	bgc.fill();
 
 	for(var i=0;i<2;i++){
-		ctx.beginPath();
-	    ctx.arc(140+i*320,140,3,0,Math.PI*2);
-	    ctx.fill();
+		bgc.beginPath();
+		bgc.fillStyle="rgb(0,0,0)";
+	    bgc.arc(140+i*320,140,3,0,Math.PI*2);
+	    bgc.fill();
 
-	    ctx.beginPath();
-	    ctx.arc(140+i*320,460,3,0,Math.PI*2);
-	    ctx.fill();
+	    bgc.beginPath();
+	    bgc.arc(140+i*320,460,3,0,Math.PI*2);
+	    bgc.fill();
 	}
 
 
-	var lingrad=ctx.createLinearGradient(20,20,20,580);
-	lingrad.addColorStop(0,'red');
-	lingrad.addColorStop(0.2,'orange');
-	lingrad.addColorStop(0.4,'yellow');
-	lingrad.addColorStop(0.6,'green');
-	lingrad.addColorStop(0.8,'blue');
-	lingrad.addColorStop(1,'purple');
+	// var lingrad=bgc.createLinearGradient(20,20,20,580);
+	// lingrad.addColorStop(0,'red');
+	// lingrad.addColorStop(0.2,'orange');
+	// lingrad.addColorStop(0.4,'yellow');
+	// lingrad.addColorStop(0.6,'green');
+	// lingrad.addColorStop(0.8,'blue');
+	// lingrad.addColorStop(1,'purple');
 
 
-	ctx.beginPath();
-	ctx.strokeStyle=lingrad;
-	ctx.lineWidth=6;
-	ctx.moveTo(20,20);
-	ctx.lineTo(20,580);
-	ctx.stroke();
+	// bgc.beginPath();
+	// bgc.strokeStyle=lingrad;
+	// bgc.lineWidth=6;
+	// bgc.moveTo(20,20);
+	// bgc.lineTo(20,580);
+	// bgc.stroke();
 
 
 	//落子
@@ -83,10 +87,31 @@ window.onload=function(){
     	var y=Math.round((e.offsetY-20.5)/40);
     	if(memory[x+'_'+y]){return}
     	luozi(x,y,kaiguan);
-    	
         memory[x+'_'+y]=kaiguan?'black':'white';
+        localStorage.kg=kaiguan;
     	kaiguan =! kaiguan;
-    	
+    	localStorage.data=JSON.stringify(memory);
+    }
+    
+    if(localStorage.kg == true){
+    	kaiguan=false;
+    }else if(localStorage.kg == false){
+    	kaiguan=true;
+    }
+    if(localStorage.data){
+    	memory=JSON.parse(localStorage.data);
+    	for(var i in memory){
+    		var x=i.split('_')[0];
+    		var y=i.split('_')[1];
+    		luozi(x,y,(memory[i]=="black")?true:false)
+    	}
+    }
+
+    resetting.onclick=function(){
+    	localStorage.clear(localStorage.data);
+    	memory={};
+    	ctx.clearRect(0,0,600,600);
+    	kaiguan=true;
     }
 	
 
